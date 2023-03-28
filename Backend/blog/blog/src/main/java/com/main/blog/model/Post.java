b5725c7856c2bec8f388002d20e7138e632488fb
Ivan Passalia <kakaroto11@live.com.ar>
2023-03-28 14:53:37 -0300

    entities crated

---
+++ Backend/blog/blog/src/main/java/com/main/blog/model/Post.java
@@ -0,0 +1,48 @@
+package com.main.blog.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "fecha", nullable = false)
+    private Date fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "image_id")
+    private List<Image> image;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinTable(name = "post_hashtag",
+           joinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "hashtag_id", referencedColumnName = "id")
+    )
+    private List<Hashtag> hashtag;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinTable(name = "post_category",
+            joinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id", referencedColumnName = "id")
+    )
+    private List<Category> categories;
+
+
+
+}
