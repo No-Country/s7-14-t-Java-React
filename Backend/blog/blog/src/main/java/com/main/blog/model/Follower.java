dbb9497295d46d4a62071b72253dc5edd55f90a1
lucero89 <laila.aquillos@gmail.com>
2023-03-29 15:24:05 -0300

    models-comment-banned-complaint

---
+++ Backend/blog/blog/src/main/java/com/main/blog/model/Follower.java
@@ -0,0 +1,28 @@
+package com.main.blog.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "followers")
+public class Follower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @JsonIgnore
+    @JoinColumn(name = "followerUser_id")
+    private User user;
+}
--- Backend/blog/blog/src/main/java/com/main/blog/model/User.java
+++ Backend/blog/blog/src/main/java/com/main/blog/model/User.java
@@ -1,9 +1,11 @@
 package com.main.blog.model;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -36,4 +38,14 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Follower> followers = new ArrayList<Follower>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Complaint> denuncias = new ArrayList<Complaint>(); //user_denuncia
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Banned> banneds = new ArrayList<Banned>();
 }
