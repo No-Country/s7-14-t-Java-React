8682a07169f12f34657e854fe2475566e6887c2e
hackermat <villacresa@gmail.com>
2023-03-30 08:34:47 -0500

    init: nav and initForm

---
+++ client/components/public/home/StartFrom/index.js
@@ -0,0 +1,11 @@
+export default function StartForm() {
+  return(
+    <div className="container">
+          <div className="row">
+            <h1>Como estamos</h1>              
+          </div>
+    </div>
+
+  )
+
+}
