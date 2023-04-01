87286e8cb14165ddc6a11ba6052286298e697eba
hackermat <villacresa@gmail.com>
2023-03-31 20:00:19 -0500

    Init: Nav

---
+++ configs/global.js
@@ -0,0 +1,24 @@
+import { css } from 'styled-components'
+
+const global = css`
+  :root {
+    --mainBg: #e8e8e8;
+    --mainBlack: #000000;
+    --primaryGreen1: #25da2d;
+    --primaryGreen2: #73e278;
+    --primaryGreen3: #97ea9b;
+    --secondaryViolet1: #120628;
+    --secondaryViolet2: #302544;
+    --secondaryViolet3: #3e2d5c;
+    --tertiaryNavy1: #6422f8;
+    --tertiaryNavy2: #8149fe;
+    --tertiaryNavy3: #a57cff;
+    --black: #222222;
+    --gray1: #454545;
+    --gray2: #636363;
+    --gray3: #818181;
+    --gray4: #ededed;
+    --errorRed: #ee0004;
+  }
+`
+export default global
