14a2a1b0ce1973843cbb00544c3f81250a4a68b7
Victorio <53796617+Victorio25@users.noreply.github.com>
2023-04-15 21:08:44 -0300

    agrego fix get imagenes

--- Backend/blog/blog/src/main/java/com/main/blog/repository/IPostRepository.java
+++ Backend/blog/blog/src/main/java/com/main/blog/repository/IPostRepository.java
@@ -4,6 +4,7 @@ import com.main.blog.model.Post;
 
 
 import com.main.blog.model.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.Optional;
 import java.util.Set;
 
 
+@Transactional
 public interface IPostRepository extends JpaRepository<Post, Long> {
     List<Post> findByCategoryId(Long categoryId);
 
